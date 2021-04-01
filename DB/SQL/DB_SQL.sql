fd599d2d96d4ff88b2165ae39887d3f383462b8b
carnival77 <stam0325@hanmail.net>
2021-04-01 16:44:10 +0900

    finishing DB sql

--- DB/SQL/DB_SQL.sql
+++ DB/SQL/DB_SQL.sql
@@ -59,7 +59,11 @@ CREATE TABLE IF NOT exists `petmeeting`.`Showoff`(
     INDEX `UID_idx` (`UID` ASC) VISIBLE,
     index `SH_ImgID_key_idx` (`ImgID` ASC) visible,
     constraint `SH_UID_key`
-		foreign key (`UID`) references `petmeeting`.`User` (`UID`),
+		foreign key (`UID`) references `petmeeting`.`User` (`UID`)
+        -- ----------------
+        ON delete restrict
+        on update restrict,
+        -- -----------------
     constraint `SH_ImgID_key`
 		foreign key (`ImgID`) references `petmeeting`.`Image` (`ImgID`)
         ON delete restrict
@@ -80,10 +84,17 @@ CREATE TABLE IF NOT exists `petmeeting`.`Vote`(
     index `SID_idx` (`SID` ASC) visible,
     constraint `VO_SID_key` 
 		foreign key (`SID`) references `petmeeting`.`Showoff` (`SID`)
-        ON DELETE cascade,
+        ON DELETE cascade
+        -- --------------------
+        on update cascade,
+        -- --------------------
 	constraint `VO_UID_key`
 		foreign key (`UID`) references `petmeeting`.`User` (`UID`)
-        on delete no action)
+        on delete no action
+        -- --------------------
+        on update cascade
+        -- --------------------
+        )
 	engine=InnoDB
     default character set = utf8mb4
     collate= utf8mb4_0900_ai_ci;
@@ -102,7 +113,11 @@ CREATE TABLE IF NOT exists `petmeeting`.`Worry`(
     index `UID_idx` (`UID` ASC) VISIBLE,
     constraint `WO_UID_key`
 		foreign key (`UID`) references `petmeeting`.`User` (`UID`)
-        on delete no action)
+        on delete no action
+        -- --------------------
+        on update cascade
+        -- --------------------
+        )
 	engine = InnoDB
 	default character set = utf8mb4
     collate= utf8mb4_0900_ai_ci;
@@ -119,10 +134,18 @@ CREATE TABLE IF NOT exists `petmeeting`.`BookMarkTo`(
     index `UID_idx` (`UID` ASC) VISIBLE,
     constraint `BO_UID_key`
 		foreign key (`UID`) references `petmeeting`.`User` (`UID`)
-        on delete cascade,
+        on delete cascade
+		-- --------------------
+        on update cascade
+        -- --------------------
+        ,
 	constraint `BO_WID_key`
 		foreign key (`WID`) references `petmeeting`.`Worry` (`WID`)
-        on delete cascade)
+        on delete cascade
+		-- --------------------
+        on update cascade
+        -- --------------------
+        )
 	engine = InnoDB
 	default character set = utf8mb4
     collate= utf8mb4_0900_ai_ci;
@@ -146,7 +169,8 @@ CREATE TABLE IF NOT exists `petmeeting`.`Comment`(
 		FOREIGN KEY (`CID_ReplyTo`)
 		REFERENCES `petmeeting`.`Comment` (`CID`)
 		ON DELETE CASCADE
-		ON UPDATE RESTRICT,
+        ON UPDATE RESTRICT
+        ,
 	  CONSTRAINT `CO_UID_key`
 		FOREIGN KEY (`UID`)
 		REFERENCES `petmeeting`.`User` (`UID`)
@@ -218,7 +242,6 @@ CREATE TABLE IF NOT exists `petmeeting`.`Entrust_Application`(
 	DEFAULT CHARACTER SET = utf8mb4
 	COLLATE = utf8mb4_0900_ai_ci;
 
-
 -- type 변환
 -- Start_Date, End_Date : date -> datetime
 -- Preypayment, Toypayment : tinyint -> int
@@ -226,25 +249,149 @@ CREATE TABLE IF NOT exists `petmeeting`.`Entrust_Application`(
     
 --------- ----------------------------------------------
 -- Table `petmeeting`.`Housings`
+-----------------------------------------------------
+DROP TABLE IF exists `petmeeting`.`Housings`;
+
+CREATE TABLE IF NOT exists `petmeeting`.`Housings`(
+	`EID` int not null,
+    `HousingID` int not null,
+    primary key(`EID`,`HousingID`),
+    index `HousingID_idx` (`HousingID` ASC) invisible,
+    constraint `HO_EID_key`
+		foreign key (`EID`) references `petmeeting`.`Entrust_Application` (`EID`)
+        on delete cascade,
+	constraint `HO_HousingID_key`
+		foreign key (`HousingID`) references `petmeeting`.`Housing`(`HousingID`))
+	ENGINE = InnoDB
+	DEFAULT CHARACTER SET = utf8mb4
+	COLLATE = utf8mb4_0900_ai_ci;
+    
 -- -----------------------------------------------------
--- DROP TABLE IF EXDD
--- CREATE TABLE IF NOT exists `petmeeting`.`Housings`(
--- 	
+-- Table `petmeeting`.`Raise_Application`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `petmeeting`.`Raise_Application` ;
 
+CREATE TABLE IF NOT EXISTS `petmeeting`.`Raise_Application` (
+	`RID` INT NOT NULL AUTO_INCREMENT primary key,
+	`Motivation` TEXT NOT NULL,
+	`Text` TEXT NOT NULL,
+	`HousingID` INT NOT NULL,
+	`EID` INT NOT NULL,
+	`UID` INT NOT NULL,
+	INDEX `EID_idx` (`EID` ASC) VISIBLE,
+	INDEX `HousingID_idx` (`HousingID` ASC) VISIBLE,
+	INDEX `UID_idx` (`UID` ASC) VISIBLE,
+	CONSTRAINT `RA_EID_key`
+		FOREIGN KEY (`EID`)
+		REFERENCES `petmeeting`.`Entrust_Application` (`EID`)
+		ON DELETE CASCADE,
+	CONSTRAINT `RA_HousingID_key`
+		FOREIGN KEY (`HousingID`)
+		REFERENCES `petmeeting`.`Housing` (`HousingID`)
+		ON DELETE RESTRICT
+		ON UPDATE RESTRICT,
+	CONSTRAINT `RA_UID_key`
+		FOREIGN KEY (`UID`)
+		REFERENCES `petmeeting`.`User` (`UID`))
+	ENGINE = InnoDB
+	DEFAULT CHARACTER SET = utf8mb4
+	COLLATE = utf8mb4_0900_ai_ci;
 
+-- -----------------------------------------------------
+-- Table `petmeeting`.`Species`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `petmeeting`.`Species` ;
 
+CREATE TABLE IF NOT EXISTS `petmeeting`.`Species` (
+	`SpeciesID` INT NOT NULL AUTO_INCREMENT,
+	`Name` VARCHAR(45) NOT NULL,
+	PRIMARY KEY (`SpeciesID`))
+	ENGINE = InnoDB
+	AUTO_INCREMENT = 4
+	DEFAULT CHARACTER SET = utf8mb4
+	COLLATE = utf8mb4_0900_ai_ci;
+
+-- -----------------------------------------------------
+-- Table `petmeeting`.`Breed`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `petmeeting`.`Breed` ;
+
+CREATE TABLE IF NOT EXISTS `petmeeting`.`Breed` (
+	`BreedID` INT NOT NULL AUTO_INCREMENT,
+	`SpeciesID` INT NOT NULL,
+	`Name` VARCHAR(45) NOT NULL,
+	PRIMARY KEY (`BreedID`),
+	INDEX `KI_Species_ID_idx` (`SpeciesID` ASC) VISIBLE,
+	CONSTRAINT `KI_Species_ID`
+		FOREIGN KEY (`SpeciesID`)
+		REFERENCES `petmeeting`.`Species` (`SpeciesID`)
+		ON DELETE CASCADE
+		ON UPDATE RESTRICT)
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+-- COMMENT = 'í’ˆì¢…';
+
+-- -----------------------------------------------------
+-- Table `petmeeting`.`Gender`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `petmeeting`.`Gender` ;
+
+CREATE TABLE IF NOT EXISTS `petmeeting`.`Gender` (
+  `GenderID` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`GenderID`))
+ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `petmeeting`.`Pet`
 -- -----------------------------------------------------
--- DROP TABLE IF exists `petmeeting`.`Pet`;
+DROP TABLE IF EXISTS `petmeeting`.`Pet` ;
 
--- CREATE TABLE IF NOT exists `petmeeting`.`Pet`(
--- 	`PID` int not null auto_increment primary key,
---     `Name` varchar(45) not null,
---     `Year` int not null,
---     `GenderID` int not null,
---     `GradeRatio` int not null,
---     `Description` Text not null,
---     `imgID` int not null,
---     `dd 
+CREATE TABLE IF NOT EXISTS `petmeeting`.`Pet` (
+  `PID` INT NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(45) NOT NULL,
+  `Year` INT NOT NULL,
+  `GenderID` INT NOT NULL,
+  `GradeRatio` INT NOT NULL,
+  `Description` TEXT NOT NULL,
+  `ImgID` INT NOT NULL,
+  `EID` INT NULL DEFAULT NULL,
+  `UID` INT NOT NULL,
+  `BreedID` INT NOT NULL,
+  PRIMARY KEY (`PID`),
+  INDEX `EID_idx` (`EID` ASC) VISIBLE,
+  INDEX `BreedID_idx` (`BreedID` ASC) VISIBLE,
+  INDEX `UID_idx` (`UID` ASC) VISIBLE,
+  INDEX `PE_ImgID_key_idx` (`ImgID` ASC) VISIBLE,
+  INDEX `PE_Gender_key_idx` (`GenderID` ASC) VISIBLE,
+  CONSTRAINT `PE_EID_key`
+    FOREIGN KEY (`EID`)
+    REFERENCES `petmeeting`.`Entrust_Application` (`EID`)
+    ON DELETE SET NULL
+    ON UPDATE RESTRICT,
+  CONSTRAINT `PE_KindID_key`
+    FOREIGN KEY (`BreedID`)
+    REFERENCES `petmeeting`.`Breed` (`BreedID`)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `PE_UID_key`
+    FOREIGN KEY (`UID`)
+    REFERENCES `petmeeting`.`User` (`UID`)
+    ON DELETE CASCADE,
+  CONSTRAINT `PE_ImgID_key`
+    FOREIGN KEY (`ImgID`)
+    REFERENCES `petmeeting`.`Image` (`ImgID`)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `PE_Gender_key`
+    FOREIGN KEY (`GenderID`)
+    REFERENCES `petmeeting`.`Gender` (`GenderID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci
+KEY_BLOCK_SIZE = 16;
